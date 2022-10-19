165fffd8e93e94832435c6de7ef17369b6a22d74
Sarah <sarahvictoria230@gmail.com>
2022-10-19 19:12:30 -0300

    aula 1 finalizada

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-import "./css/index.css"
+import "./css/index.css";
+import IMask from "imask"
+
+const ccBgColor1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
+const ccBgColor2 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
+const logo = document.querySelector(".cc-logo span:nth-child(2) img");
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    default: ["black", "gray"],
+  };
+
+  ccBgColor1.setAttribute("fill", colors[type][0]);
+  ccBgColor2.setAttribute("fill", colors[type][1]);
+  logo.setAttribute("src", `cc-${type}.svg`);
+};
+setCardType("visa");
+
+globalThis.setCardType();
