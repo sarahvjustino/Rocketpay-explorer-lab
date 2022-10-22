719207fa1f168407bea2e80df4123c3516f14f8a
Sarah <sarahvictoria230@gmail.com>
2022-10-21 22:41:48 -0300

    Adição de novos cartões

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ function setCardType(type) {
   const colors = {
     visa: ["#436D99", "#2D57F2"],
     mastercard: ["#DF6F29", "#C69347"],
+    americanexpress: ['#0E8BFF', '#FA0EFF'],
+    maestro: ['#FF3C21', '#FD294F'],
     default: ["black", "gray"],
   };
 
@@ -57,6 +59,16 @@ const cardNumberPattern = {
       mask: "0000 0000 0000 0000",
       regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
       cardtype: "mastercard",
+    },
+    {
+      mask: '0000 000000 00000',
+      regex: /^3[47]\d{0,13}/,
+      cardtype: 'americanexpress'
+    },
+    {
+      mask: '0000 0000 0000 0000',
+      regex: /^(?:5[0678]\d{0,2}|6304|67\d{0,2})\d{0,12}/,
+      cardtype: 'maestro'
     },
     {
       mask: "0000 0000 0000 0000",
